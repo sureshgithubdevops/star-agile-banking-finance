dacf5e6c40d90c87ca4132fc23244246e99094b9
suri <sureshalivelu99@gmail.com>
2023-04-20 20:01:13 +0000

    modify jenkins file and new deployments

---
+++ deployments/main.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-0c768662cc797cd75" 
+  instance_type = "t2.micro" 
+  key_name = "bhem"
+  vpc_security_group_ids= ["sg-09865566f5f0fa10a"]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("./bhem.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "test-server"
+  }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+		}
+  provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/project-2\ bank/deployments/bankplaybook.yml"
+  } 
+}
+
---
+++ deployments/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA274V3I3LOWVN5XVY"
+  secret_key = "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"
+}
